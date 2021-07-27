79181abf3c957076ae8b0650c0199cb6453f9e85
Alexandre Moreau-Lemay <alexandre.moreau-lemay@aml-design.com>
2021-07-27 15:03:00 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alexandre Moreau-Lemay on 27/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
